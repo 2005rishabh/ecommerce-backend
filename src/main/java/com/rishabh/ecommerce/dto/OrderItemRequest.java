d77b48f3c17f6e93cb08db691ed465987197d7bb
Rishabh Singh <20rishabh05@gmail.com>
2026-06-04 01:14:23 +0530

    Add OrderRequest and OrderItemRequest DTOs for order processing; define fields for user ID and order items

---
+++ src/main/java/com/rishabh/ecommerce/dto/OrderItemRequest.java
@@ -0,0 +1,21 @@
+package com.rishabh.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+
+public class OrderItemRequest {
+    private Long productId;
+    private Long quantity;
+}
--- src/main/java/com/rishabh/ecommerce/dto/OrderRequest.java
+++ src/main/java/com/rishabh/ecommerce/dto/OrderRequest.java
@@ -3,26 +3,23 @@ package com.rishabh.ecommerce.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.rishabh.ecommerce.entities.Status;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 
-public class OrderResponse {
-    private long id;
-    private String orderNumber;
-    private int totalAmount;
-    private Status status;
-    private List<OrderItemResponse> items;
+public class OrderRequest {
+    private Long userId;
+    private List<OrderItemRequest> items;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
