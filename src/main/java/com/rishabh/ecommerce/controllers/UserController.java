c611b3662028500455cd92eeb552e9dfb892f868
Rishabh Singh <20rishabh05@gmail.com>
2026-06-03 22:27:16 +0530

    Implement UserController with CRUD operations for users, including create, retrieve, update, and delete functionalities

---
+++ src/main/java/com/rishabh/ecommerce/controllers/UserController.java
@@ -0,0 +1,60 @@
+package com.rishabh.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rishabh.ecommerce.dto.ProductResponse;
+import com.rishabh.ecommerce.dto.UserRequest;
+import com.rishabh.ecommerce.dto.UserResponse;
+import com.rishabh.ecommerce.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
+        UserResponse response = userService.createUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> gellAllUsers() {
+        List<UserResponse> listRepo = userService.getAllProducts();
+        return ResponseEntity.ok(listRepo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(Long id) {
+        UserResponse response = userService.getUserById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(Long id, UserRequest request) {
+        UserResponse response = userService.updateProductDetails(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+}
