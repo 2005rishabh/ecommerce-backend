d764e0b633be32629c41aed2005b7d7abdf5c61a
Rishabh Singh <20rishabh05@gmail.com>
2026-06-05 21:18:33 +0530

    Enhance Order and OrderItem entities with relationships; update OrderService and OrderServiceImpl for order retrieval and management

--- src/main/java/com/rishabh/ecommerce/entities/Order.java
+++ src/main/java/com/rishabh/ecommerce/entities/Order.java
@@ -1,25 +1,31 @@
 package com.rishabh.ecommerce.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,6 +34,7 @@ import lombok.Setter;
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "orders")
 @Getter
+@Data
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -52,6 +59,9 @@ public class Order {
     @NotNull(message = "must mention what is the role of person")
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     @CreatedDate
     @Column(name = "created_at", nullable = false)
--- src/main/java/com/rishabh/ecommerce/services/OrderService.java
+++ src/main/java/com/rishabh/ecommerce/services/OrderService.java
@@ -12,5 +12,7 @@ public interface OrderService {
 
     OrderResponse getOrderById(Long id);
 
+    OrderResponse updateOrder(Long id, OrderRequest request, Long reqUserId); // i have to make sure only admin is allowed to update order
+
     void deleteOrder(Long id);
 }
