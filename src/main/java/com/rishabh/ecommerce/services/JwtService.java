ccd0a1eacbe769cac8dacafa81f3808bc06a9784
Rishabh Singh <20rishabh05@gmail.com>
2026-06-12 20:24:59 +0530

    Refactor JwtService to improve code readability and add token validation logic

--- src/main/java/com/rishabh/ecommerce/services/JwtService.java
+++ src/main/java/com/rishabh/ecommerce/services/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
@@ -18,24 +19,48 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String secretString;
 
-    //its main job is to create the key
+    // its main job is to create the key
     private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode((secretString));
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-
-    //key will be used here for generating token(main use of compact())
+    // key will be used here for generating token(main use of compact())
     public String generateToken(String username) {
         return Jwts.builder()
-        .subject(username)
-        .issuedAt(new Date(System.currentTimeMillis()))
-        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6))
-        .signWith(getSigningKey())
-        .compact();
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6))
+                .signWith(getSigningKey())
+                .compact();
 
     }
+    
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
-    //When a request comes in with a token, they crack the token open to read the data inside.
+    // When a request comes in with a token, they crack the token open to read the
+    // data inside.
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject(); // extract username
+    }
 
+    
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+        return expiration.before(new Date()); //it will checks token time against curr time
+    }
 }
